70de8934a041ed34713b358d2fcfe36c4c76e241
Timothy Lee <marlboromoo@gmail.com>
2014-01-14 19:47:10 +0800

    Check master branch status before commit to gh-pages.

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -z $(git status | grep 'nothing to commit')  ]]; then
+    echo "Commit first! Abort."
+    exit 0
+fi
+
 export LANG=en; make clean; make html
 rm -rf /tmp/output/
 mv output/ /tmp/
