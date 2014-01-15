9f882495f58aa9633b5cdfd1fc188b416e78521f
Timothy Lee <marlboromoo@gmail.com>
2014-01-15 14:09:44 +0800

    Update feed settings.

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,7 +18,8 @@ FAVICON = 'images/favicon.ico'
 #SITEBRAND = 'images/MoosWorld_silver.png'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_DOMAIN = SITEURL
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
